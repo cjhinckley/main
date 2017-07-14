a98f1a7c1fdc140f3776ac5aaf201935a64a3494
A Carter Hinckley <CJHinckley@As-MacBook-Air.local>
2017-07-14 14:13:26 -0400

    push brooklyn

--- w1d5/do_now/startup/apps.js
+++ w1d5/do_now/startup/apps.js
@@ -1,1 +1,63 @@
-console.log("we are locked and loaded");
+// The Fortune Teller
+// function tellFortune(children, partner, location, job) {
+// const result = "You will be a" + job + " in" + location + ", and married to" + partner + " with" + children + " children."; 
+// return result; 
+// }
+//  const answer = tellFortune(5, " Trump", " Vermont", " doctor");
+//  alert(answer);
+
+// Dog Age
+// function calc(age) {
+//     const dogYears = 7 * age;
+//     console.log("Your doge is " + dogYears + " years old in dog years!")
+// }
+// calc(12);
+
+// Lifetime supply
+// function calculateSupply(age, numPerDay) {
+//   const maxAge = 100;
+//   const totalNeeded = (numPerDay * 365) * (maxAge - age);
+//   const message = 'You will need ' + totalNeeded + ' cups of tea to last you until the ripe old age of ' + maxAge;
+//   alert(message);
+// }
+
+// calculateSupply(13, 36);
+// calculateSupply(13, 2.5);
+// calculateSupply(13, 400);
+
+//Geometrizer
+// function calcCircum(radius){
+// 	const circum = (radius*2)*Math.PI;
+// 	alert ("circumference is "+circum);
+// }
+// calcCircum(12);
+
+// function calcarea(radius){
+// 	const area = (radius*radius*Math.PI)
+// 	alert ("area is "+area)
+// }
+// calcarea(15)
+
+//Tempconvert
+function ctof(c){
+	const f = (c*(9/5)+32)
+	alert ("it is "+f+"º")
+}
+ctof(10)
+
+function ftoc(f){
+	const c = ((f-32)/(9/5))
+	alert ("it is "+c+"º")
+}
+ftoc(13)
+
+
+
+
+
+
+
+
+
+
+
